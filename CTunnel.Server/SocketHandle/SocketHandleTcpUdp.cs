ecef8bcfa8e4248cd21179cb85d5ae840e855030
hzc <2635174704@qq.com>
2025-06-15 22:04:59 +0800

    refactor: 转发逻辑重构

--- CTunnel.Server/SocketHandle/SocketHandleTcpUdp.cs
+++ CTunnel.Server/SocketHandle/SocketHandleTcpUdp.cs
@@ -1,11 +1,9 @@
 ﻿using System.Buffers;
 using System.Net.Sockets;
 using System.Net.WebSockets;
-using System.Text;
-using CTunnel.Share;
-using CTunnel.Share.Enums;
 using CTunnel.Share.Expand;
 using CTunnel.Share.Model;
+using Newtonsoft.Json;
 
 namespace CTunnel.Server.SocketHandle;
 
@@ -13,65 +11,35 @@ public class SocketHandleTcpUdp(TunnelContext tunnelContext) : ISocketHandle
 {
     public async Task HandleAsync(Socket socket, int port)
     {
-        // Console.WriteLine("新连接");
-        // socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
-        // var socketStream = await socket.GetStreamAsync(false, true, string.Empty);
-        // using var memory = MemoryPool<byte>.Shared.Rent(GlobalStaticConfig.BufferSize);
-        // var tunnel = tunnelContext.GetTunnel(port.ToString());
-        // if (tunnel == null)
-        //     return;
-        // var requestItem = new RequestItem()
-        // {
-        //     RequestId = [],
-        //     TargetSocket = socket,
-        //     TargetSocketStream = socketStream,
-        // };
-        // tunnel.ConcurrentDictionary.TryAdd(requestItem.Id, requestItem);
-        // try
-        // {
-        //     var newRequestId = Guid.NewGuid().ToString();
-        //     requestItem.RequestId.TryAdd(newRequestId, 0);
-        //     // 请求的第一波数据
-        //     await tunnel.ForwardToClientSlim.WaitAsync();
-        //     await tunnel.WebSocket.SendAsync(
-        //         newRequestId.ToBytes(),
-        //         WebSocketMessageType.Binary,
-        //         false,
-        //         CancellationToken.None
-        //     );
-        //     await tunnel.WebSocket.SendAsync(
-        //         ArraySegment<byte>.Empty,
-        //         WebSocketMessageType.Binary,
-        //         false,
-        //         CancellationToken.None
-        //     );
-        //     int readCount;
-        //     TaskExtend.NewTask(async () =>
-        //     {
-        //         await Task.Delay(10000);
-        //         await socket.TryCloseAsync();
-        //     });
-        //     while ((readCount = await socketStream.ReadAsync(memory.Memory)) != 0)
-        //     {
-        //         await tunnel.WebSocket.SendAsync(
-        //             memory.Memory[..readCount],
-        //             WebSocketMessageType.Binary,
-        //             false,
-        //             CancellationToken.None
-        //         );
-        //     }
-        // }
-        // finally
-        // {
-        //     await tunnel.WebSocket.SendAsync(
-        //         ArraySegment<byte>.Empty,
-        //         WebSocketMessageType.Binary,
-        //         true,
-        //         CancellationToken.None
-        //     );
-        //     tunnel.ForwardToClientSlim.Release();
-        //     Console.WriteLine("连接结束");
-        //     await requestItem.CloseAsync(tunnel.ConcurrentDictionary);
-        // }
+        // 新的Socket连接
+        socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+        var socketStream = await socket.GetStreamAsync(false, true, string.Empty);
+        var memory = MemoryPool<byte>.Shared.Rent(0);
+        var tunnel = tunnelContext.GetTunnel(port.ToString());
+        if (tunnel == null)
+            return;
+        var requestItem = new RequestItem()
+        {
+            TargetSocket = socket,
+            TargetSocketStream = socketStream,
+            ToBeSent = memory,
+            ToBeSentCount = 0
+        };
+        tunnel.ConcurrentDictionary.TryAdd(requestItem.Id, requestItem);
+
+        // 发送新连接通知
+        var registerRequest = new RegisterRequest
+        {
+            TunnelKey = tunnel.Key,
+            RequestId = requestItem.Id
+        };
+        var bytes = JsonConvert.SerializeObject(registerRequest).ToBytes();
+        await tunnel.WebSocket.SendAsync(
+            bytes,
+            WebSocketMessageType.Binary,
+            true,
+            CancellationToken.None
+        );
+        await Task.Delay(Timeout.InfiniteTimeSpan, requestItem.TokenSource.Token);
     }
 }
